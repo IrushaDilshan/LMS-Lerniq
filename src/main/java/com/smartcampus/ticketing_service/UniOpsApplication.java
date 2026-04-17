d5c4a49fe5cdc23fa0abb84ac099975afb22b8e8
IrushaDilshan <it23768758@my.sliit.lk>
2026-04-17 20:38:55 +0530

    Cleanup generated files and rename app class
    Remove a large set of generated/debug artefacts and unused frontend code: deleted compile logs, debug/log/output/compile_error files, helper scripts (convert.py, print_compile_err.py), frontend assets and services (App.css, react.svg, vite.svg, assignmentService.js, quizService.js), test/upload example files, and other extraneous artifacts. Also rename the main application class from TicketingServiceApplication.java to UniOpsApplication.java to reflect the updated app name. This cleans the repo of bulky/unused files and updates the application entrypoint name.

--- src/main/java/com/smartcampus/ticketing_service/UniOpsApplication.java
+++ src/main/java/com/smartcampus/ticketing_service/UniOpsApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketingServiceApplication {
+public class UniOpsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TicketingServiceApplication.class, args);
+		SpringApplication.run(UniOpsApplication.class, args);
 	}
-	
-
 }
